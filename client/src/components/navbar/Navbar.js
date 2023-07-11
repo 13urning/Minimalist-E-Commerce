2c44fc981e61b8977dbcceaacfa0b6f58d462fab
Regor Carlo <esconderegor@gmail.com>
2023-07-11 13:33:18 +0800

    added mobile nav bar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Badge from "@mui/material/Badge";
 
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 export default function Navbar() {
   const [userScroll, setUserScroll] = useState(false);
+  const [mobMenu, setMobMenu] = useState(false);
   const handleScroll = () => {
     if (window.scrollY > 10) {
       setUserScroll(true);
@@ -27,6 +29,19 @@ export default function Navbar() {
 
   return (
     <>
+      <div className={mobMenu ? "showMenuNav" : "hideMenuNav"}>
+        <div
+          className="right-6 top-12 absolute cursor-pointer"
+          onClick={() => setMobMenu(!mobMenu)}
+        >
+          <CloseRoundedIcon />
+        </div>
+        <div className="uppercase flex flex-col gap-8 justify-center absolute top-[44] left-[50] text-3xl text-center">
+          <Link to="/categories/all">categories</Link>
+          <Link to="/categories/lamps">lamps</Link>
+          <Link to="/categories/product/1">featured product</Link>
+        </div>
+      </div>
       <nav
         className={`sticky top-0 w-full text-xl z-[200] bg-white items-center shadow-md flex px-10 py-5 justify-between ${
           userScroll ? "h-20" : "h-24"
@@ -52,11 +67,33 @@ export default function Navbar() {
               <ShoppingCartOutlinedIcon />
             </Badge>
           </li>
-          <li className="text-3xl md:hidden">
+          <li
+            className="text-3xl md:hidden"
+            onClick={() => setMobMenu((prev) => !prev)}
+          >
             <MenuRoundedIcon />
           </li>
         </ul>
       </nav>
+      <style>{`
+        .hideMenuNav{
+            display: none;
+        }
+        .showMenuNav{
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100vh;
+            top: 0;
+            left: 0;
+            background: white;
+            z-index: 250;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+    `}</style>
     </>
   );
 }
