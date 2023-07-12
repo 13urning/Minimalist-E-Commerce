6e4ff5ca9f1f3e56de580bec1f8cbfdb052d2c0e
Regor Carlo <esconderegor@gmail.com>
2023-07-12 11:03:05 +0800

    updated hero block to have responsive design

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function Hero() {
   return (
     <>
-      <div className="pt-48">
-        <div className="container">
-          <div className="grid h-auto grid-col-4 grid-rows-4 gap-4">
-            <div className="overflow-hidden relative cursor-pointer grid-one">
+      <div className="pt-12">
+        <div className="container mx-auto">
+          <div className="grid h-[32rem] grid-cols-2 gap-4 mt-5 md:grid-cols-5 md:grid-rows-2">
+            <div className="overflow-hidden relative cursor-pointer md:row-span-2 md:col-span-3 ">
               <Link to="categories/furnitures">
                 <div className="absolute w-full h-full top-0 left-0 bg-slate-950/25"></div>
                 <img
@@ -15,12 +15,12 @@ export default function Hero() {
                   alt="img1"
                   className="h-full w-full object-cover"
                 />
-                <p className="absolute bottom-4 left-4 text-md">
+                <p className="absolute bottom-4 left-4 text-2xl">
                   Live Comfortably
                 </p>
               </Link>
             </div>
-            <div className="overflow-hidden relative cursor-pointer grid-two">
+            <div className="overflow-hidden relative cursor-pointer md:row-span-2">
               <Link to="categories/skin-care">
                 <div className="absolute w-full h-full top-0 left-0 bg-slate-950/25"></div>
                 <img
@@ -28,10 +28,10 @@ export default function Hero() {
                   alt=""
                   className="h-full w-full object-cover"
                 />
-                <p className="absolute bottom-4 left-4 text-md">Skin care</p>
+                <p className="absolute bottom-4 left-4 text-2xl">Skin care</p>
               </Link>
             </div>
-            <div className="row-start-2 overflow-hidden relative cursor-pointer">
+            <div className="md:col-start-5 overflow-hidden relative cursor-pointer">
               <Link to="categories/kitchen">
                 <div className="absolute w-full h-full top-0 left-0 bg-slate-950/25"></div>
                 <img
@@ -39,10 +39,10 @@ export default function Hero() {
                   alt=""
                   className="h-full w-full object-cover"
                 />
-                <p className="absolute bottom-4 left-4 text-md">Kitchen</p>
+                <p className="absolute bottom-4 left-4 text-2xl">Kitchen</p>
               </Link>
             </div>
-            <div className="row-start-2 overflow-hidden relative cursor-pointer">
+            <div className=" md:col-start-5 overflow-hidden relative cursor-pointer">
               <Link to="categories/electronics">
                 <div className="absolute w-full h-full top-0 left-0 bg-slate-950/25"></div>
                 <img
@@ -50,7 +50,7 @@ export default function Hero() {
                   alt=""
                   className="h-full w-full object-cover"
                 />
-                <p className="absolute bottom-4 left-4 text-md">Electronics</p>
+                <p className="absolute bottom-4 left-4 text-2xl">Electronics</p>
               </Link>
             </div>
           </div>
