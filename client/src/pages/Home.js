96a7359d427a694a505851d1e5d6fbcde1614a1f
Regor Carlo <esconderegor@gmail.com>
2023-07-18 08:13:57 +0800

    added banner block for home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
 import FeaturedProducts from "../components/Featured/FeaturedProducts";
+import Banner from "../components/Banner/Banner";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FeaturedProducts />
+      <Banner
+        title="Creative harmonious living"
+        text="BRAND Products are all made to standard sizes so that you can mix and match them freely"
+        img="https://placehold.co/300x300?text=BANNER"
+      />
     </>
   );
 }
