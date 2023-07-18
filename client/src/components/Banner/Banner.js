3bfe7dbb502b34d82620789f5e0b6d78e3f1bc58
Regor Carlo <esconderegor@gmail.com>
2023-07-18 09:03:16 +0800

    added styles and responsiveness for banner block

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -3,20 +3,31 @@ import { Link } from "react-router-dom";
 
 export default function Banner({ title, text, img }) {
   return (
-    <div className="banner">
+    <div className="pb-40 px-6">
       <div className="container mx-auto">
-        <div className="banner-container">
-          <div className="text-side">
+        <div className="flex justify-between h-[40rem] overflow-hidden">
+          <div className="w-full text-start flex flex-col justify-center items-center px-[4rem] py-0 md:px-[11rem] bg-[#e9e9e9]">
             <div className="text">
-              <h2>{title}</h2>
-              <p>{text}</p>
+              <h2 className="text-[2.5rem] md:text-[3rem] font-semibold mb-6">
+                {title}
+              </h2>
+              <p className="text-[1.2rem] md:text-[1.6rem]">{text}</p>
               <Link onClick={() => window.scrollTo(0, 0)} to="categories/all">
-                <button>Shop Now</button>
+                <button
+                  className="bg-black text-white text-xl md:text-[1.3rem] font-bold p-3 md:p-4 uppercase mt-8 cursor-pointer outline-black outline
+                 transition-all ease-in duration-150 hover:bg-transparent hover:text-black"
+                >
+                  Shop Now
+                </button>
               </Link>
             </div>
           </div>
-          <div className="img-side">
-            <img src={img} alt="banner" />
+          <div className="hidden md:block w-full h-full object-cover">
+            <img
+              src={img}
+              alt="banner"
+              className="w-full h-full object-cover"
+            />
           </div>
         </div>
       </div>
